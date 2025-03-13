7bad0ce92e8b35258cd8b1884528d5152df78db0
Ihar <rathack13@gmail.com>
2025-03-13 23:24:45 +0300

    added: base no-logic routes server for auth

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,9 @@ const compression = require('compression')
 const helmet = require('helmet')
 const cors = require('cors')
 const morgan = require("morgan")
+const router = require('./routes/index')
 
+// SERVER
 const app = express()
 app.use(cors()) 
 app.use(bodyParser.urlencoded({extended: true})) 
@@ -15,11 +17,20 @@ app.use(bodyParser.json())
 app.use(compression());  // Response compression by different types 
 app.use(helmet()); // Defense; from the XSS attacks
 app.use(morgan("combined", { stream: logger.stream }))
+app.use(router)
+// app.use(errorHandler)
 
+
+// MONGO
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => logger.info('MongoDB connected'))
-  .catch(error => console.error(error))
+  .catch(err => logger.error(err))
 
-require('./bot/bot');
+// BOT
+try {
+  require('./bot/bot');
+} catch (err) {
+  logger.error(err);
+}
 
 module.exports = app
---
+++ server/src/controllers/auth.js
@@ -0,0 +1,10 @@
+
+
+module.exports.login = async function(req, res) {
+
+}
+
+
+module.exports.register = async function(req, res) {
+
+}
