34083c85edf7a6cbafe28549bf06512e3944de56
Ihar <rathack13@gmail.com>
2025-03-20 11:07:28 +0300

    updated: shop page

--- client/src/pages/ShopPage.jsx
+++ client/src/pages/ShopPage.jsx
@@ -3,18 +3,21 @@ import "../styles/pages/Shop.css";
 import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
 import api from '../utils/api.js';
+import { useDispatch } from 'react-redux';
+import { setCoins } from '../redux/coinsSlice.jsx';
 
 
 const Shop = () => {
   const [items, setItems] = useState([]);
-  const [hoveredItem, setHoveredItem] = useState(null);
-  const userId = sessionStorage.getItem("userId")
+  const [userItems, setUserItems] = useState([]);
+  const dispatch = useDispatch();
+  const userId = sessionStorage.getItem("userId");
+
 
   useEffect(() => {
-
     const fetchItems = async () => {
       try {
-        console.log("fetch all items from shop")
+        console.log("fetch all items from shop");
         const response = await api.get("/shop/items");
         setItems(response.data);
       } catch (error) {
@@ -22,49 +25,58 @@ const Shop = () => {
       }
     };
 
+    const fetchUserItems = async () => {
+      try {
+        console.log(`fetch user ${userId} items`);
+        const response = await api.get(`/user/${userId}/items`);
+        setUserItems(response.data.items);
+      } catch (error) {
+        console.error("Ошибка загрузки предметов пользователя:", error);
+      }
+    };
+
     fetchItems();
+    fetchUserItems();
   }, []);
 
-  const handleBuy = async (itemId) => {
+
+  const handleAction = async (itemId, isOwned) => {
     try {
-      const response = await api.post(`/user/items`, {
-        userId: userId,
-        itemId,
-      });
-      console.log(`Add item ${itemId} to user ${userId}`)
+      const transactionResponse = await api.post("/user/items", { userId, itemId });
+      console.log(transactionResponse)
+      console.log(`${isOwned ? "Upgrade" : "Buy"} item ${itemId} for user ${userId}`);
+      // Обновляем список предметов пользователя после покупки/улучшения
+      const responseUserItems = await api.get(`/user/${userId}/items`);
+      dispatch(setCoins(transactionResponse.data.userBalance)); // Обновляем монеты в Redux
+      setUserItems(responseUserItems.data.items);
     } catch (error) {
-      console.error("Ошибка при покупке предмета:", error);
+      console.error(`Ошибка при ${isOwned ? "улучшении" : "покупке"} предмета:`, error);
     }
   };
+
 
   return (
     <>
       <div className="shop-container">
-        {items.map((item) => (
-          <div key={item._id} className="shop-item">
-            <div className="item-image-container">
-              <img src={item.imageUrl} alt={item.name} className="item-image" />
+        {items.map((item) => {
+            const userItem = userItems.find(userItem => userItem.itemId._id === item._id);
+          const isOwned = !!userItem;
+          const price = isOwned ? userItem.upgradePrice : item.basePrice;
+
+          return (
+            <div key={item._id} className="shop-item">
+              <div className="item-image-container">
+                <img src={item.imageUrl} alt={item.name} className="item-image" />
+              </div>
+              <div className="item-info">
+                <h3 className="item-title">{item.name}</h3>
+              </div>
+              <button className="buy-button" onClick={() => handleAction(item._id, isOwned)}>
+                {isOwned ? "Улучшить" : "Купить"} {price}₿
+              </button>
             </div>
-            <div className="item-info">
-              <h3 className="item-title">{item.name}</h3>
-              {/*<div className="tooltip-wrapper">*/}
-              {/*  <button*/}
-              {/*    className="info-button"*/}
-              {/*    onMouseEnter={() => setHoveredItem(item._id)}*/}
-              {/*    onMouseLeave={() => setHoveredItem(null)}*/}
-              {/*  >*/}
-              {/*    <span>?</span>*/}
-              {/*  </button>*/}
-              {/*  {hoveredItem === item._id && (*/}
-              {/*    <div className="tooltip">{item.description}</div>*/}
-              {/*  )}*/}
-              {/*</div>*/}
-            </div>
-            <button className="buy-button" onClick={() => handleBuy(item._id)}>
-              Купить за {item.price}₿
-            </button>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </>
 
